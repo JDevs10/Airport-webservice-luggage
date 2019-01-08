890571db3ba3d7d8cd8219abba9e7bd28e9c5518
Jean-Laurent Duzant <j-ldsxm18@hotmail.com>
2019-01-08 11:44:10 +0100

    ---------- Airport WebService Luggage ----------
    - suite du tp "Global"

---
+++ ClassLibrary1/WindowsFormsApp1/Pim.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class PIM
+    {
+        private PimState state = PimState.Deconnecter;
+        private PimState State
+        {
+            get { return this.state; }
+            set { OnPimStateChanged(value); }
+        }
+
+        public enum PimState
+        {
+            Deconnecter,
+            AttenteBagage,
+            SelectionBagage,
+            CreationBagage,
+            AffichageBagage
+        }
+
+        public event PimStateEventHandler PimStateChanged;
+        public delegate void PimStateEventHandler(object sender, PimState state);
+
+        private void OnPimStateChanged(PimState newState)
+        {
+            if (newState != this.state)
+            {
+                this.state = newState;
+                if (this.PimStateChanged != null)
+                {
+                    PimStateChanged(this, this.state);
+                }
+            }
+        }
+
+        public PIM()
+        {
+            InitializeComponent();
+            this.PimStateChanged += PIM_PimStateChanged;
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        void PIM_PimStateChanged(object sender, PimState state)
+        {
+            MessageBox.Show("StateChanged");
+        }
+    }
+}
